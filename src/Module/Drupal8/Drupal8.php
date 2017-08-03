2dedd43cc0911e0bde95dd9699ec6e06532634cb
JvE <joris.vaneijden@oneshoe.nl>
2017-08-03 12:53:45 +0200

    Only initialize module once also for D8.

--- src/Module/Drupal8/Drupal8.php
+++ src/Module/Drupal8/Drupal8.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Codeception Drupal 8 bootstrap.
- */
-
 namespace Codeception\Module\Drupal8;
 
 use Codeception\Module\DrupalBaseModule;
@@ -14,22 +9,30 @@ use Drupal\Core\Site\Settings;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * Class Drupal8
+ * Class Drupal8.
+ *
  * @package Codeception\Module\Drupal8
  */
 class Drupal8 extends DrupalBaseModule implements DrupalModuleInterface {
 
+  private $initialized;
+
   /**
-   * { @inheritdoc }
+   * {@inheritdoc}
+   *
+   * @throws \Codeception\Exception\DrupalNotFoundException
    */
   public function _initialize() {
 
-    $this->bootstrapDrupal();
+    if (!$this->initialized) {
+      $this->bootstrapDrupal();
+      $this->initialized = TRUE;
+    }
 
   }
 
   /**
-   * { @inheritdoc }
+   * {@inheritdoc}
    */
   public function bootstrapDrupal() {
     $this->config['root'] = $this->getDrupalRoot();
@@ -38,7 +41,6 @@ class Drupal8 extends DrupalBaseModule implements DrupalModuleInterface {
     if (!defined('DRUPAL_ROOT')) {
       define('DRUPAL_ROOT', $this->config['root']);
     }
-    chdir(DRUPAL_ROOT);
 
     // Get drupal site.
     $site = isset($this->config['site_dir']) && !empty($this->config['site_dir']) ? "sites/{$this->config['site_dir']}" : 'sites/default';
@@ -58,13 +60,14 @@ class Drupal8 extends DrupalBaseModule implements DrupalModuleInterface {
   }
 
   /**
-   * { @inheritdoc }
+   * {@inheritdoc}
    */
   public function validateDrupalRoot($root) {
     if (!file_exists($root . '/autoload.php') || !file_exists($root . '/core/includes/bootstrap.inc')) {
       throw new DrupalNotFoundException('Drupal not found at ' . $root . '.');
     }
 
-    return true;
+    return TRUE;
   }
+
 }
--- src/Module/DrupalBaseModule.php
+++ src/Module/DrupalBaseModule.php
@@ -1,34 +1,30 @@
-<?php namespace Codeception\Module;
+<?php
 
-use Codeception\Module;
+namespace Codeception\Module;
 
-abstract class DrupalBaseModule extends Module
-{
-    /**
-     * @var array
-     */
-    protected $config = [
-      'root' => null
-    ];
+abstract class DrupalBaseModule extends Module {
 
-    /**
-     * Get the Drupal root directory.
-     *
-     * @return string
-     *   The root directory of the Drupal installation.
-     */
-    public function getDrupalRoot()
-    {
-        // We can't get getcwd() as a default parameter, so this will have to do.
-        if (is_null($this->config['root'])) {
-            return codecept_root_dir();
-        }
+  protected $config = ['root' => NULL];
 
-        // Allow a user to pass an relative or an absolute path.
-        if (is_null($this->config['relative']) || $this->config['relative'] !== 'yes') {
-            return $this->config['root'];
-        } else {
-            return codecept_root_dir($this->config['root']);
-        }
+  /**
+   * Get the Drupal root directory.
+   *
+   * @return string
+   *   The root directory of the Drupal installation.
+   */
+  public function getDrupalRoot() {
+    // We can't get getcwd() as a default parameter, so this will have to do.
+    if ($this->config['root'] === NULL) {
+      $rootDir = codecept_root_dir();
     }
+    // Allow a user to pass an relative or an absolute path.
+    elseif (isset($this->config['relative']) && $this->config['relative'] === 'yes') {
+      $rootDir = codecept_root_dir($this->config['root']);
+    }
+    else {
+      $rootDir = $this->config['root'];
+    }
+    return $rootDir;
+  }
+
 }
